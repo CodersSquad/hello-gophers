db4e665fa34d87ebb175f1dca286d5e3ff501fdb
Obed N Munoz <obed.n.munoz@gmail.com>
2019-10-15 17:10:49 -0500

    Update content
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- src/00/webServer.go
+++ src/00/webServer.go
@@ -11,7 +11,7 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-// handler echoe the Path component of the requested URL.
+// handler echo the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
